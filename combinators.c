d82dcf69af193b4b7c56d206e1b3ea92ebc6ca6a
Carlos Martin <carlos.martin@columbia.edu>
2016-03-27 20:06:37 -0400

    Created tree data structure for expressions

--- combinators.c
+++ combinators.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+typedef enum { leaf, branch } Type;
+typedef struct Tree {
+	Type type;
+	union {
+		int value;
+		struct {
+			struct Tree * left;
+			struct Tree * right;
+		};
+	};
+} Tree;
+
 int main() {
 	printf("Hello world!\n");
 	return 0;
